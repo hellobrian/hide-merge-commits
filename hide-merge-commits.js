81ba71cdb4a4b68751cad83fe476314f0ec8df7b
thisisbrianhan <bthan@us.ibm.com>
2016-02-22 21:17:55 -0600

    Refactor
    Variables

--- hide-merge-commits.js
+++ hide-merge-commits.js
@@ -1,15 +1,34 @@
-var commits = document.querySelectorAll('.commit-title a');
+var commits = document.querySelectorAll('.commit-title .message');
 
 var hideMergeCommits = function(nodeList) {
   for (var i=0; i<nodeList.length; i++) {
-    nodeList[i].text.split(' ').forEach(function(word){
-      if (word === 'Merge') {
-        console.log(nodeList[i].parentNode.parentNode.parentNode);
-        nodeList[i].parentNode.parentNode.parentNode.style.display = "none";
-      } else {
-        console.log('else');
-      }
-    });
+
+    var text = nodeList[i].text.split(' ').shift().toLowerCase();
+    var parent = nodeList[i].parentNode.parentNode.parentNode;
+
+    parent.style.transition = '200ms ease-in-out';
+
+    if (text === 'merge') {
+      // parent.style.opacity = '.3';
+      parent.style.display = 'none';
+    }
+
+    else if (text === 'bump') {
+      parent.style.backgroundColor = 'rgba(39, 174, 96, 0.5)';
+    }
+
+    else {
+      parent.style.backgroundColor = 'rgba(241, 196, 15, 0.25)';
+    }
+
+
+
+    // else {
+    //   parent.style.opacity = '.05';
+    //   parent.style.background = 'white !important';
+    // }
+
+
   }
 }
 
